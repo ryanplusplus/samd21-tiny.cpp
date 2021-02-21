b597eb968dafcbefec25434dffa1fcc7f1729c50
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-21 14:14:33 -0500

    Marked hardware_manual target as PHONY

--- docs.mk
+++ docs.mk
@@ -1,6 +1,7 @@
 HARDWARE_MANUAL_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/SAM_D21_DA1_Family_DataSheet_DS40001882F.pdf
 DATASHEET_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/SAM_D21_DA1_Family_DataSheet_DS40001882F.pdf
 
+.PHONY: hardware_manual
 hardware_manual:
 	xdg-open $(HARDWARE_MANUAL_URL)
 
