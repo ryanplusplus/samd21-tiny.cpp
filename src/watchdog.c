f642ff97f42cc7a476ef9844aca653702f7ad965
Ryan Hartlage <ryanplusplus@gmail.com>
2021-05-02 15:50:53 -0400

    Watchdog configuration without ASF

--- src/watchdog.c
+++ src/watchdog.c
@@ -6,26 +6,43 @@
 #include "watchdog.h"
 #include "wdt.h"
 
+#define write_and_sync(...)                        \
+  do {                                             \
+    __VA_ARGS__                                    \
+    while(WDT->STATUS.reg & WDT_STATUS_SYNCBUSY) { \
+    }                                              \
+  } while(0)
+
 static tiny_timer_t timer;
 
 static void kick(tiny_timer_group_t* _timer_group, void* context)
 {
   (void)context;
   (void)_timer_group;
-  wdt_reset_count();
+  WDT->CLEAR.reg = WDT_CLEAR_CLEAR_KEY;
 }
 
 void watchdog_init(tiny_timer_group_t* timer_group)
 {
-  struct wdt_conf config = {
-    .always_on = false,
-    .enable = true,
-    .clock_source = GCLK_GENERATOR_1, // ULP 32KHz
-    .timeout_period = WDT_PERIOD_16384CLK, // 0.5 seconds
-    .window_period = WDT_PERIOD_NONE,
-    .early_warning_period = WDT_PERIOD_NONE
-  };
-  wdt_set_config(&config);
+  // Enable WDT clock
+  PM->APBAMASK.reg |= PM_APBAMASK_WDT;
+
+  // Select GCLK1 (ULP32K)
+  write_and_sync({
+    GCLK->CLKCTRL.reg =
+      GCLK_CLKCTRL_ID(WDT_GCLK_ID) |
+      GCLK_CLKCTRL_GEN_GCLK1 |
+      GCLK_CLKCTRL_CLKEN;
+  });
+
+  // Set period to 16384 ticks (~0.5 seconds at 32.758 kHz)
+  write_and_sync({
+    WDT->CONFIG.reg = WDT_CONFIG_PER_11;
+  });
+
+  write_and_sync({
+    WDT->CTRL.reg |= WDT_CTRL_ENABLE;
+  });
 
   tiny_timer_start_periodic(timer_group, &timer, 250, kick, NULL);
 }
--- src/watchdog.h
+++ src/watchdog.h
@@ -6,8 +6,6 @@
 #ifndef watchdog_h
 #define watchdog_h
 
-#include "wdt.h"
-
 #include "tiny_timer.h"
 
 void watchdog_init(tiny_timer_group_t* timer_group);
