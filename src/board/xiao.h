8b7ba5a1ae9621e412efa636fc6145de94ec8957
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-09 19:26:17 -0500

    DMA-driven async SPI for xiao

--- src/board/xiao.h
+++ src/board/xiao.h
@@ -15,12 +15,14 @@
 #include "uart_sercom4_pb08_pb09.h"
 #include "buffered_uart_sercom4_pb08_pb09.h"
 #include "spi_sercom0_pa05_pa06_a07.h"
+#include "async_spi_sercom0_pa05_pa06_a07.h"
 #include "i2c_sercom2_pa08_pa09.h"
 
 #define heartbeat_init heartbeat_pa17_init
 #define uart_init uart_sercom4_pb08_pb09_init
 #define buffered_uart_init buffered_uart_sercom4_pb08_pb09_init
 #define spi_init spi_sercom0_pa05_pa06_a07_init
+#define async_spi_init async_spi_sercom0_pa05_pa06_a07_init
 #define i2c_init i2c_sercom2_pa08_pa09_init
 
 #endif
--- src/main.c
+++ src/main.c
@@ -8,14 +8,6 @@
 
 static tiny_timer_group_t timer_group;
 
-static void finished(void* context)
-{
-  (void)context;
-  volatile int x;
-  x = 4;
-  (void)x;
-}
-
 int main(void)
 {
   interrupts_disable();
@@ -26,12 +18,6 @@ int main(void)
     heartbeat_init(&timer_group);
   }
   interrupts_enable();
-
-  uint8_t write_buffer[] = { 0, 1, 2, 3, 4, 5, 6 };
-  uint8_t read_buffer[sizeof(write_buffer)];
-  i_tiny_async_spi_t* spi = async_spi_init(1, 0, 1, 1000000);
-  tiny_async_spi_transfer(spi, write_buffer, read_buffer, sizeof(write_buffer), NULL, finished);
-  read_buffer[6] = 0xFF;
 
   while(1) {
     if(!tiny_timer_group_run(&timer_group)) {
